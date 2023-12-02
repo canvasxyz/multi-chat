b34cf288825561d0a8c069f162ca5a3cbe6a9ea1
Raymond Z <raykyri@gmail.com>
2023-12-02 17:14:53 -0500

    add canvas-chat-3 replication server

--- packages/client/src/Chat.tsx
+++ packages/client/src/Chat.tsx
@@ -51,6 +51,10 @@ export const ChatInstance = ({ topic, left }) => {
       topic,
     },
     signers: [new SIWESigner({ signer })],
+    bootstrapList: [
+      "/dns4/canvas-chat-3.fly.dev/tcp/443/wss/p2p/12D3KooWCQQz7uozb287GZCRGv7DrrZTVDuUfh2bNCd3rpUHgpes",
+      ...defaultBootstrapList,
+    ],
   })
   const messages = useLiveQuery<Message>(app, "message", {
     orderBy: { timestamp: "asc" },
