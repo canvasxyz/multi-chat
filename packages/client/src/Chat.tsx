e83104a0ec88a5a6043c6ad27ba5e979e0f645cb
Raymond Z <raykyri@gmail.com>
2023-12-02 18:11:38 -0500

    enable webrtc

--- packages/client/src/Chat.tsx
+++ packages/client/src/Chat.tsx
@@ -55,6 +55,7 @@ export const ChatInstance = ({ topic, left }) => {
       "/dns4/canvas-chat-3.fly.dev/tcp/443/wss/p2p/12D3KooWCQQz7uozb287GZCRGv7DrrZTVDuUfh2bNCd3rpUHgpes",
       ...defaultBootstrapList,
     ],
+    enableWebRTC: true,
   })
   const messages = useLiveQuery<Message>(app, "message", {
     orderBy: { timestamp: "asc" },
