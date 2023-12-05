6094b0be3ac92d25c1d3b346937a051105b0d30c
Raymond Z <raykyri@gmail.com>
2023-12-04 22:03:26 -0500

    remove testing peer

--- packages/client/src/Chat.tsx
+++ packages/client/src/Chat.tsx
@@ -85,7 +85,6 @@ export const ChatInstance = ({
     indexHistory: false,
     discoveryTopic: "canvas-discovery",
     bootstrapList: [
-      "/dns4/peer.canvasjs.org/tcp/443/wss/p2p/12D3KooWFYvDDRpXtheKXgQyPf7sfK2DxS1vkripKQUS2aQz5529",
       "/dns4/canvas-chat-discovery-p0.fly.dev/tcp/443/wss/p2p/12D3KooWG1zzEepzv5ib5Rz16Z4PXVfNRffXBGwf7wM8xoNAbJW7",
       "/dns4/canvas-chat-discovery-p1.fly.dev/tcp/443/wss/p2p/12D3KooWNfH4Z4ayppVFyTKv8BBYLLvkR1nfWkjcSTqYdS4gTueq",
       "/dns4/canvas-chat-discovery-p2.fly.dev/tcp/443/wss/p2p/12D3KooWRBdFp5T1fgjWdPSCf9cDqcCASMBgcLqjzzBvptjAfAxN",
