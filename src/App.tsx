b59aba3d2d3b120ce16f8b2e355ba53399d7c668
Raymond Z <raykyri@gmail.com>
2024-07-29 16:31:12 -0400

    extract multi chat package

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+import { Chat } from "./Chat"
+import { PrivateKeyAccount, privateKeyToAccount, generatePrivateKey } from "viem/accounts"
+import { Hex } from "viem"
+
+const styleUnset = { all: "unset" } as const
+
+export const App = () => {
+	const [account, setAccount] = useState<PrivateKeyAccount | null>(null)
+	useEffect(() => {
+		const privkey = localStorage.getItem("burnerWallet")
+		if (privkey !== null && privkey.startsWith("0x")) {
+			try {
+				setAccount(privateKeyToAccount(privkey as Hex))
+				return
+			} catch (err) {}
+		} 
+		const newPrivkey = generatePrivateKey()
+		localStorage.setItem("burnerWallet", newPrivkey)
+		setAccount(privateKeyToAccount(newPrivkey))
+	}, [])
+	useState
+
+	return <>{account && <Chat account={account} />}</>
+}
