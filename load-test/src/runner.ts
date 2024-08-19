101f07beb90d5efa18fcf10e9f6665ed195e7119
Raymond Z <raykyri@gmail.com>
2024-08-19 17:55:27 -0400

    load-test: use a separate page in puppeteeer for each topic

--- load-test/src/runner.ts
+++ load-test/src/runner.ts
@@ -4,9 +4,29 @@ import puppeteer from "puppeteer"
 
 import { Canvas } from "@canvas-js/core"
 
+type Config = {
+	topic: string
+	numTopics: number
+	cycleUp: number
+	cycleDown: number
+}
+
+type Stats = {
+	peerCount: number
+	messageCount: number
+	lastMessageCount: number
+	clock: number
+}
+
+type Room = {
+	start: () => void
+	topic: string
+} & Stats
+
 declare global {
 	function log(...args: any[]): void
-	function getConfig(): { numTopics: number; cycleUp: number; cycleDown: number }
+	function getConfig(): Config
+	function updateStats(stats: Stats): void
 	// @ts-ignore
 	var _multiaddr: multiaddr
 	var _Canvas: Canvas
@@ -16,111 +36,85 @@ const numTopics = process.env.NUM_TOPICS ? parseInt(process.env.NUM_TOPICS) : 10
 const cycleUp = process.env.UP ? parseInt(process.env.UP) : 120
 const cycleDown = process.env.DOWN ? parseInt(process.env.DOWN) : 60
 
+// loadTest is stringified and executed inside puppeteer, so all shadowed variables
 const loadTest = async () => {
-	const { numTopics, cycleUp, cycleDown } = await getConfig()
+	const { topic, numTopics, cycleUp, cycleDown } = await getConfig()
 	const apiRoot = "http://localhost:3000"
 
-	let sending = true
+	let peers: string[] = []
+	let lastMessages: number = 0
 
-	/*
-	 * Set up `apps` and `peers`, and initialize `numTopics` apps
-	 * (always one app for each topic) and connect them to the server
-	 */
-	const peers: Record<string, string[]> = {}
-	const apps: Record<string, Canvas> = {}
-	const messages: Record<string, number> = {}
-
-	for (let i = 0; i < numTopics; i++) {
-		try {
-			const topic = `room-${i}.canvas.xyz`
-			const app = (await (_Canvas as any).initialize({
-				topic,
-				contract: {
-					models: {},
-					actions: {
-						createMessage: () => {},
-					},
+	try {
+		const app = (await (_Canvas as any).initialize({
+			topic,
+			contract: {
+				models: {},
+				actions: {
+					createMessage: () => {},
 				},
-				start: false,
-				bootstrapList: [],
-			})) as Canvas
-			apps[i] = app
+			},
+			start: false,
+			bootstrapList: [],
+		})) as Canvas
 
-			app.libp2p.addEventListener("connection:open", ({ detail: connection }) => {
-				peers[i] = peers[i] || []
-				peers[i] = Array.from(new Set([...peers[i], connection.remotePeer.toString()]))
-			})
+		app.libp2p.addEventListener("connection:open", ({ detail: connection }) => {
+			peers = Array.from(new Set([...peers, connection.remotePeer.toString()]))
+		})
 
-			app.libp2p.addEventListener("connection:close", ({ detail: connection }) => {
-				peers[i] = peers[i] || []
-				peers[i] = peers[i].filter((peer: string) => peer !== connection.remotePeer.toString())
-			})
+		app.libp2p.addEventListener("connection:close", ({ detail: connection }) => {
+			peers = peers.filter((peer: string) => peer !== connection.remotePeer.toString())
+		})
 
-			const connect = async (app: Canvas) => {
-				try {
-					const res = await fetch(`${apiRoot}/topic/${topic}`)
-					const { addrs }: { addrs: string[] } = await res.json()
-					await app.libp2p.dial(addrs.map((addr) => _multiaddr(addr)))
-				} catch (err) {
-					console.error(err)
-				}
+		const connect = async (app: Canvas) => {
+			try {
+				const res = await fetch(`${apiRoot}/topic/${topic}`)
+				const { addrs }: { addrs: string[] } = await res.json()
+				await app.libp2p.dial(addrs.map((addr) => _multiaddr(addr)))
+			} catch (err) {
+				console.error(err)
 			}
-
-			Promise.resolve(app.libp2p.start())
-				.then(() => {
-					connect(app)
-
-					const interval = setInterval(() => {
-						const peers = app.libp2p.getPeers()
-						if (peers.length === 0) {
-							connect(app)
-						}
-					}, 5000)
-
-					app.libp2p.addEventListener("stop", () => clearInterval(interval), { once: true })
-				})
-				.catch((err) => {
-					log(err.stack)
-				})
-
-			apps[i] = app
-
-			let j = 0
-			setInterval(async () => {
-				if (sending) {
-					await app.actions.createMessage({ content: (j++).toString() })
-				}
-			}, 1000)
-		} catch (err: any) {
-			log(err.stack)
 		}
+
+		Promise.resolve(app.libp2p.start())
+			.then(() => {
+				connect(app)
+
+				const interval = setInterval(() => {
+					const peers = app.libp2p.getPeers()
+					if (peers.length === 0) {
+						connect(app)
+					}
+				}, 5000)
+
+				app.libp2p.addEventListener("stop", () => clearInterval(interval), { once: true })
+			})
+			.catch((err) => {
+				log(err.stack)
+			})
+
+		let j = 0
+		setInterval(async () => {
+			await app.actions.createMessage({ content: (j++).toString() })
+
+			// pass stats up through puppeteer
+			const messages = await app.messageLog.db.count("$messages")
+			const [clock] = await app.messageLog.getClock()
+			updateStats({
+				peerCount: peers.length,
+				messageCount: messages,
+				lastMessageCount: lastMessages,
+				clock,
+			})
+			lastMessages = messages
+		}, 1000)
+	} catch (err: any) {
+		log(err.stack)
 	}
-
-	let elapsed = 0
-	setInterval(async () => {
-		log(`${elapsed++} seconds elapsed`)
-
-		for (let i = 0; i < numTopics; i++) {
-			const online = peers[i].length > 0 ? "🟢" : "🔴"
-			const topic = `room-${i}.canvas.xyz`
-			const msgs = await apps[i].messageLog.db.count("$messages")
-			const lastMsgs = messages[i]
-			messages[i] = msgs
-			const [clock] = await apps[i].messageLog.getClock()
-			log(`${online} ${topic}: ${msgs} messages, ${msgs - lastMsgs} msgs/sec [${clock} clock]`)
-		}
-		log(`active: ${sending}`)
-
-		if (elapsed % (cycleUp + cycleDown) <= cycleUp) {
-			sending = true
-		} else {
-			sending = false
-		}
-	}, 1000)
 }
 
-const setupPage = async (page: puppeteer.Page, bundle: string) => {
+const setupPage = async (page: puppeteer.Page, bundle: string, topic: string, updateStats: (room: Room) => void) => {
 	await page.setRequestInterception(true)
+
 	page.on("request", async (request) => {
 		try {
 			if (request.method() === "GET") {
@@ -143,13 +137,22 @@ const setupPage = async (page: puppeteer.Page, bundle: string) => {
 	})
 
 	page.on("console", (msg) => {
-		console.log(`[${msg.type()}] ${msg.text()}`)
+		console.log(`[${topic}] ${msg.text()}`)
 	})
 
-	await page.goto("http://localhost:3000")
+	try {
+		await page.goto("http://localhost:3000")
+	} catch (err) {
+		console.log("Could not reach server")
+		throw err
+	}
 
 	await page.exposeFunction("getConfig", () => {
-		return { numTopics, cycleUp, cycleDown }
+		return { numTopics, cycleUp, cycleDown, topic }
+	})
+
+	await page.exposeFunction("updateStats", (stats: Room) => {
+		updateStats(stats)
 	})
 
 	await page.exposeFunction("log", (...args: any[]) => console.log(...args))
@@ -180,9 +183,37 @@ const runner = async () => {
 			"--enable-chrome-browser-cloud-management",
 		],
 	})
-	const page = await browser.newPage()
-	const start = await setupPage(page, bundle)
-	await start()
+
+	const roomIds = Array.from(Array(10).keys())
+	const roomTopics = roomIds.map((i) => `room-${i}.canvas.xyz`)
+	const rooms: Room[] = await Promise.all(
+		roomTopics.map(async (topic) => {
+			const page = await browser.newPage()
+			const room = { start: () => {}, topic, messageCount: 0, lastMessageCount: 0, clock: 0, peerCount: 0 }
+			room.start = await setupPage(page, bundle, topic, (stats: Stats) => {
+				room.messageCount = stats.messageCount
+				room.lastMessageCount = stats.lastMessageCount
+				room.clock = stats.clock
+				room.peerCount = stats.peerCount
+			})
+			return room
+		}),
+	)
+
+	rooms.map((room) => room.start())
+
+	let elapsed = 0
+	setInterval(async () => {
+		console.log(`${elapsed++} seconds elapsed`)
+
+		for (const room of rooms) {
+			const online = room.peerCount > 0 ? "🟢" : "🔴"
+			const { messageCount, lastMessageCount, clock, topic } = room
+			console.log(
+				`${online} ${topic}: ${messageCount} messages, ${messageCount - lastMessageCount} msgs/sec [${clock} clock]`,
+			)
+		}
+	}, 1000)
 }
 
 runner()
